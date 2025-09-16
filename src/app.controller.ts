aded409d7890036ccad6c8a43d5955fd82da6b73
yasvizeu <yasminvizeusantana@yahoo.com>
2025-09-16 18:15:37 -0300

    build

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
+
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    const dbUser = this.configService.get<string>('DB_USER')
+   // return this.appService.getHello();
+   return `Usuário do banco: ${dbUser}`
   }
 }
